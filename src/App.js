d7515066ed5bb74e388b242adec67b6336878ac7
Shankar Aswal <shankaraswal@gmail.com>
2024-01-16 16:14:52 +0000

    removed lgo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>GITHUB ACTIONS POC</h1>
+      <h1>GITHUB ACTIONS POC from master</h1>
       <p>{process.env.REACT_APP_TEXT || "Github Actions poc"} - By ss aswal</p>
     </div>
   );
